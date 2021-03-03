3299eccb010cc43628ce2437ef1c37e4002d2699
AdrianEka <adrian.eka9@gmail.com>
2021-03-03 10:46:48 +0700

    Add fitur Menambah dan Menampilkan Daftar Menu

--- AplikasiRestauran/src/Main/MainAplikasiKasir.java
+++ AplikasiRestauran/src/Main/MainAplikasiKasir.java
@@ -1,6 +1,41 @@
 package Main;
 
+import Classes.*;
+
+import java.util.Scanner;
+
 public class MainAplikasiKasir {
-    public static void main(String[] args){}
-    public void generateDaftarMenu() {}
+    public static void main(String[] args){
+        //inisialisasi kelas Scanner untuk mengambil
+        //input dari keyboard
+        Scanner input = new Scanner(System.in);
+
+        MainAplikasiKasir app = new MainAplikasiKasir();
+        //tampilan daftar menu
+        app.generateDaftarMenu();
+    }
+    public DaftarMenu daftarMenu;
+    public void generateDaftarMenu() {
+        daftarMenu = new DaftarMenu();
+        daftarMenu.tambahMenu(new Ramen("Ramen Seafood", 25000));
+        daftarMenu.tambahMenu(new Ramen("Ramen Original", 18000));
+        daftarMenu.tambahMenu(new Ramen("Ramen Vegetarian", 22000));
+        daftarMenu.tambahMenu(new Ramen("Ramen Karnivor",  28000));
+        daftarMenu.tambahMenu(new Kuah("Kuah Original "));
+        daftarMenu.tambahMenu(new Kuah("Kuah Internasional"));
+        daftarMenu.tambahMenu(new Kuah("Kuah Spicy Lada"));
+        daftarMenu.tambahMenu(new Kuah("Kuah Soto Padang"));
+        daftarMenu.tambahMenu(new Toping("Crab Stick Bakar", 6000));
+        daftarMenu.tambahMenu(new Toping("Chicken Katsu", 8000));
+        daftarMenu.tambahMenu(new Toping("Gyoza Goreng", 4000));
+        daftarMenu.tambahMenu(new Toping("Bakso Goreng", 7000));
+        daftarMenu.tambahMenu(new Toping("Enoki Goreng", 5000));
+        daftarMenu.tambahMenu(new Minuman("Jus Alpukat SPC", 10000));
+        daftarMenu.tambahMenu(new Minuman("Jus Stroberi", 11000));
+        daftarMenu.tambahMenu(new Minuman("Capucino Coffee", 15000));
+        daftarMenu.tambahMenu(new Minuman("Vietnam Dripp", 14000));
+
+        daftarMenu.tampilDaftarMenu();
+    }
+
 }
